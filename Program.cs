b1f51d778d8a358c275c306bc8e4498fbdad9fc3
guilherme domingos <guilherme.santos240503@gmail.com>
2025-03-23 16:56:21 -0300

    terminado

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿    
+         Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("=== Letra da Música: Planos - BK' ===");
+        Console.ResetColor();
+        Console.WriteLine();
+
+        Console.WriteLine("Eu faço planos, não curto promessas");
+        Console.WriteLine("É muito mais prático quando você pensa e age");
+        Console.WriteLine("Mas tem que ter coragem pra encarar a caminhada");
+        Console.WriteLine("Ou então se conforma e desiste na metade");
+
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("\nSei que vão falar, mas não vão me parar");
+        Console.WriteLine("Eu vou chegar lá, porque eu tenho fé");
+        Console.WriteLine("Sei que vão falar, mas não vão me parar");
+        Console.WriteLine("Eu vou chegar lá, porque eu tenho fé");
+        Console.ResetColor();
+
+       
+        Console.WriteLine("\nQuero minha conta cheia e minha mente sã");
+        Console.WriteLine("Paz pra minha família e vida longa pros irmãos");
+        Console.WriteLine("Porque eu sei que a luta nunca vai ser em vão");
+        Console.WriteLine("E quem tem fé não tem medo da escuridão");
+
+   
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("\nSei que vão falar, mas não vão me parar");
+        Console.WriteLine("Eu vou chegar lá, porque eu tenho fé");
+        Console.WriteLine("Sei que vão falar, mas não vão me parar");
+        Console.WriteLine("Eu vou chegar lá, porque eu tenho fé");
+        Console.ResetColor();
+
+       
+        Console.WriteLine("\nEu faço planos, não curto promessas");
+        Console.WriteLine("É muito mais prático quando você pensa e age");
+        Console.WriteLine("Mas tem que ter coragem pra encarar a caminhada");
+        Console.WriteLine("Ou então se conforma e desiste na metade");
+
+     
+        Console.WriteLine("\nPressione qualquer tecla para sair...");
+        Console.ReadKey();
